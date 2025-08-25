14233dcd1db4426581efe81142bbe7382c63d67e
Nikhil Bhalotia <nikhilbhalotia21@gmail.com>
2025-08-25 17:42:18 +0000

    o1_basic_react

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h1>chai aur code | Nikhil: I learned in this video about how to manuplate the core reactAPP</h1>
+  );
+}
+
+export default App;
---
+++ 01_basic_react/src/New.js
@@ -0,0 +1,7 @@
+function New() {
+    return (
+        <h1>I am learning how to create components in react js</h1>
+    )
+}
+
+export default New
